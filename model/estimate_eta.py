4a0627bb5b7ce61ce39035901d9e48d87dca025f
Choono <49roinevery@gmail.com>
2024-02-22 23:40:39 -0500

    Update estimate_eta.py

--- model/estimate_eta.py
+++ model/estimate_eta.py
@@ -3,6 +3,10 @@
 Created on Mon Feb 19 18:09:07 2024
 
 @author: jbk5816
+
+Estimate descent direction \eta from input x. 
+x consists of [real(s), imag(s), real(w_m), imag(w_m), v_m],
+where v_m denotes the mapping vector for m-th target
 """
 
 import torch
@@ -12,7 +16,7 @@ import torch.nn as nn
 class Estimate_eta(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(Estimate_eta, self).__init__()
-        # Define layers for estimating the real part of v
+        # Define layers for estimating descent direction eta
         self.layers = nn.Sequential(
             nn.Linear(input_dim, 1024),
             nn.ReLU(),
@@ -22,19 +26,6 @@ class Estimate_eta(nn.Module):
             nn.ReLU(),
             nn.Linear(256, output_dim),  # Output dimension matches phi 128
         )
-        # # Define layers for estimating the imaginary part of v
-        # self.imag_layers = nn.Sequential(
-        #     nn.Linear(input_dim, 256),
-        #     nn.ReLU(),
-        #     nn.Linear(256, 128),
-        #     nn.ReLU(),
-        #     nn.Linear(128, input_dim),  # Output dimension matches phi
-        # )
 
     def forward(self, x):
         return self.layers(x)
-        # real = self.real_layers(s)
-        # imag = self.imag_layers(s)
-        # # Combine real and imag parts to form the complex-valued vector v
-        # v = torch.complex(real, imag)
-        # return v
