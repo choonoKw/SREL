55b081b5a3563b8a7cbf39f406ca3c37e0ff8f4f
Choono <49roinevery@gmail.com>
2024-02-22 23:41:16 -0500

    Create train_regular_1step.py
    training with reulgarized loss: SREL 1 step

---
+++ trash/train_regular_1step.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 16:06:41 2024
+
+@author: jbk5816
+"""
+import torch
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Subset
+
+from utils.complex_valued_dataset import ComplexValuedDataset
+from utils.load_scalars_from_setup import load_scalars_from_setup
+from utils.load_mapVector import load_mapVector
+from model.srel_model_reg_1step import SREL
+
+from utils.custom_loss import sum_of_reciprocal, regularizer_eta
+from utils.worst_sinr import worst_sinr_function
+
+from visualization.plotting import plot_losses
+
+def main():
+    # Load dataset
+    constants = load_scalars_from_setup('data/data_setup.mat')
+    v_M, Lv = load_mapVector('data/data_mapV.mat')
+    dataset = ComplexValuedDataset('data/data_trd_1e1.mat')
+    
+    # Split dataset into training and validation
+    train_indices, val_indices = train_test_split(
+        range(len(dataset)),
+        test_size=0.2,  # 20% for validation
+        random_state=42
+    )
+    train_dataset = Subset(dataset, train_indices)
+    val_dataset = Subset(dataset, val_indices)
+    
+    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+    test_loader = DataLoader(val_dataset, batch_size=2, shuffle=False)
+    
+    # defining constant
+    constants["Lv"] = Lv
+    Nt = constants['Nt']
+    N = constants['N']
+    modulus = 1 / torch.sqrt(torch.tensor(Nt * N, dtype=torch.float))
+
+    # Initialize model
+    model = SREL(constants)
+    num_epochs = 10
+    # Initialize the optimizer
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    lambda_eta = 0.0001
+    
+    # List to store average loss per epoch
+    training_losses = []
+    validation_losses = []
+
+    # Training loop
+    for epoch in range(num_epochs):
+        model.train()  # Set model to training mode
+        total_train_loss = 0.0
+        
+        
+        for phi_batch, w_M_batch, G_M_batch, H_M_batch  in train_loader:
+            # Perform training steps
+            # for m in range(1,constants['M']+1):
+            optimizer.zero_grad()
+            s_batch = modulus * torch.exp(1j * phi_batch)
+            
+            phi_optimal_batch, eta_M_batch = model(phi_batch, w_M_batch, v_M)
+            s_optimal_batch = modulus * torch.exp(1j * phi_optimal_batch)
+            
+            # calculate loss
+            primary_loss = sum_of_reciprocal(constants, s_optimal_batch, G_M_batch, H_M_batch)
+            regularization_loss = regularizer_eta(constants, s_batch, G_M_batch, H_M_batch, eta_M_batch)
+            
+            loss = primary_loss + lambda_eta * regularization_loss
+        
+            loss.backward()
+            optimizer.step()
+            
+            total_train_loss += loss.item()
+        
+            
+        # Compute average loss for the epoch
+        average_train_loss = total_train_loss / len(train_loader)
+        training_losses.append(average_train_loss)
+        
+        
+        # Validation phase
+        model.eval()  # Set model to evaluation mode
+        total_val_loss = 0.0
+        
+        sum_of_worst_sinr = 0.0  # Accumulate loss over all batches
+        
+        with torch.no_grad():  # Disable gradient computation
+            for phi_batch, w_M_batch, G_M_batch, H_M_batch in test_loader:
+                s_batch = modulus * torch.exp(1j * phi_batch)
+                
+                phi_optimal_batch, eta_M_batch = model(phi_batch, w_M_batch, v_M)
+                s_optimal_batch = modulus * torch.exp(1j * phi_optimal_batch)
+                
+                # calculate loss
+                primary_loss = sum_of_reciprocal(constants, s_optimal_batch, G_M_batch, H_M_batch)
+                regularization_loss = regularizer_eta(constants, s_batch, G_M_batch, H_M_batch, eta_M_batch)
+                
+                val_loss = primary_loss + lambda_eta * regularization_loss
+                total_val_loss += val_loss.item()
+                
+                sum_of_worst_sinr += worst_sinr_function(constants, s_optimal_batch, G_M_batch, H_M_batch)
+        # validation_losses.append(total_val_loss / len(test_loader))
+        
+        
+        
+        # Store the average loss for this epoch
+        average_val_loss = total_val_loss / len(test_loader)
+        validation_losses.append(average_val_loss)
+        
+        average_worst_sinr_db = 10*torch.log10(sum_of_worst_sinr/ len(test_loader))  # Compute average loss for the epoch
+        print(f'Epoch [{epoch+1}/{num_epochs}], '
+              # f'Train Loss = {average_train_loss:.4f}, '
+              f'average_worst_sinr = {average_worst_sinr_db:.4f} dB')
+        
+    # After completing all epochs, plot the training loss
+    plot_losses(training_losses, validation_losses)
+    
+if __name__ == "__main__":
+    main()
+
