f49319f078d17ba4445da7baffee19e40a9f5c98
Choono <49roinevery@gmail.com>
2024-02-28 14:07:30 -0500

    Separate files for -intra and -inter

---
+++ model/srel_intra.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 12:09:49 2024
+
+@author: jbk5816
+"""
+
+from model.estimate_eta import Estimate_eta
+from model.estimate_rho import Estimate_rho
+import torch
+import torch.nn as nn
+
+class SREL_intra(nn.Module):
+    def __init__(self, constants):
+        super(SREL_intra, self).__init__()
+        # Unpack constants from the dictionary and store as attributes
+        self.M = constants['M']
+        self.Ls = constants['Nt']*constants['N']
+        self.N_step = constants['N_step']
+        self.modulus = constants['modulus']
+        
+        # Dynamically create the modules for estimating eta and rho
+        self.est_eta_modules = nn.ModuleList([
+            Estimate_eta(2*self.Ls + 2*constants['Lw'] + constants['Ly'], self.Ls)
+            for _ in range(self.N_step)
+        ])
+        self.est_rho_modules = nn.ModuleList([
+            Estimate_rho(2*self.Ls + 2*constants['Lw'] + constants['Ly'], 1)
+            for _ in range(self.N_step)
+        ])
+        
+    def forward(self, phi0_batch, w_batch, y):
+        batch_size = phi0_batch.size(0)
+        N_step = self.N_step
+        modulus = self.modulus
+        
+        # Initialize the list
+        s_stack_batch = torch.zeros(batch_size, N_step+1, self.Ls, dtype=torch.complex64)
+        eta_stack_batch = torch.zeros(batch_size, N_step, self.Ls) 
+        
+        for idx_batch in range(batch_size):
+            phi0 = phi0_batch[idx_batch]
+            w = w_batch[idx_batch]
+            
+            
+            # Repeat the update process N_step times
+            phi = phi0
+            for update_step in range(N_step):
+                s = modulus*torch.exp(1j *phi)
+                
+                x = torch.cat((s.real, s.imag, w.real, w.imag, y), dim=0)
+                eta = self.est_eta_modules[update_step](x)
+                beta = self.est_rho_modules[update_step](x)  # Remove extra dimensions
+                
+                phi = phi - beta*eta  # Update phi
+                
+                # save on list
+                s_stack_batch[idx_batch,update_step,:] = s
+                eta_stack_batch[idx_batch,update_step,:] = eta
+            
+            s_stack_batch[idx_batch,N_step,:] = modulus*torch.exp(1j *phi)  # Saving the final s after all updates
+            
+        model_outputs = {
+            's_stack_batch': s_stack_batch,
+            'eta_stack_batch': eta_stack_batch
+        }
+        return model_outputs
---
+++ train_inter.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 14:06:24 2024
+
+@author: jbk5816
+"""
+
