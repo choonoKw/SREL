c916eb299567ccc0cd47e0bfc9b4ce24bf8982aa
Choono <49roinevery@gmail.com>
2024-02-29 11:47:53 -0500

    Update srel_inter.py
    Use rho for intra-step-size, rather than beta that is used for interference power.

--- model/srel_inter.py
+++ model/srel_inter.py
@@ -37,7 +37,7 @@ class SREL_inter(nn.Module):
         
         # Initialize the list
         s_stack_batch = torch.zeros(batch_size, N_step+1, self.Ls, dtype=torch.complex64)
-        eta_stack_batch = torch.zeros(batch_size, N_step, self.Ls) 
+        # eta_stack_batch = torch.zeros(batch_size, N_step, self.Ls) 
         
         for idx_batch in range(batch_size):
             phi0 = phi0_batch[idx_batch]
@@ -55,18 +55,19 @@ class SREL_inter(nn.Module):
                     y = y_M[:,m]
                     x = torch.cat((s.real, s.imag, w.real, w.imag, y), dim=0)
                     eta = self.model_intra.est_eta_modules[update_step](x)
-                    beta = self.model_intra.est_rho_modules[update_step](x)
+                    rho = self.model_intra.est_rho_modules[update_step](x)
                     mu = self.est_mu_modules[update_step](x)
                     
-                    eta_net += mu*beta*eta
+                    eta_net += mu*rho*eta
                 
                 
                 
-                phi = phi - beta*eta_net  # Update phi
+                phi = phi - eta_net  # Update phi
                 
                 # save on list
+                
                 s_stack_batch[idx_batch,update_step,:] = s
-                eta_stack_batch[idx_batch,update_step,:] = eta
+                # eta_stack_batch[idx_batch,update_step,:] = eta
             
             s_stack_batch[idx_batch,N_step,:] = modulus*torch.exp(1j *phi)  # Saving the final s after all updates
             
