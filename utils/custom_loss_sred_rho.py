2cb834c2934c35d9d058cb44dd5b165eb2bcb191
Choono <49roinevery@gmail.com>
2024-03-27 01:21:59 -0400

    Update custom_loss_sred_rho.py
    We do not need to compute variance

--- utils/custom_loss_sred_rho.py
+++ utils/custom_loss_sred_rho.py
@@ -13,7 +13,7 @@ from utils.custom_loss_intra import reciprocal_sinr
 def custom_loss_function(constants, G_M_batch, H_M_batch, hyperparameters, model_outputs):
     N_step = constants['N_step']
     s_stack_batch = model_outputs['s_stack_batch']
-    rho_M_stack_batch = model_outputs['rho_M_stack_batch']
+    # rho_M_stack_batch = model_outputs['rho_M_stack_batch']
     batch_size = s_stack_batch.size(0)
     
     loss_sum = 0.0
@@ -42,11 +42,13 @@ def custom_loss_function(constants, G_M_batch, H_M_batch, hyperparameters, model
             f_sinr_opt += reciprocal_sinr(G, H, s)
             
             
-        # compute the variance of rho
-        rho_M_stack = rho_M_stack_batch[idx_batch]
-        var_rho_avg = torch.sum(torch.var(rho_M_stack, dim=0, unbiased=False))
+        # # compute the variance of rho
+        # rho_M_stack = rho_M_stack_batch[idx_batch]
+        # var_rho_avg = torch.sum(torch.var(rho_M_stack, dim=0, unbiased=False))
     
-        loss = f_sinr_opt + hyperparameters['lambda_sinr']*f_sinr/(N_step-1) + hyperparameters['lambda_var_rho']*var_rho_avg
+        loss = f_sinr_opt + hyperparameters['lambda_sinr']*f_sinr/(N_step-1)
+        # + hyperparameters['lambda_var_rho']*var_rho_avg
+        
             
         loss_sum += loss
     
