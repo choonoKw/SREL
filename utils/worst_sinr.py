b349597816a98025e52d05c2e0b1bdec88b8e593
Choono <49roinevery@gmail.com>
2024-03-28 09:40:05 -0400

    Update worst_sinr.py
    Output is batch, not the average value

--- utils/worst_sinr.py
+++ utils/worst_sinr.py
@@ -6,13 +6,15 @@ Created on Thu Feb 22 14:09:40 2024
 """
 
 import torch
+import numpy as np
 
 def worst_sinr_function(constants, s_optimal_batch, G_M_batch, H_M_batch):
     batch_size = s_optimal_batch.size(0)
     M = constants['M']
 
     # Process each item in the batch
-    sum_worst_sinr = 0.0
+    # sum_worst_sinr = 0.0
+    worst_sinr_batch = np.zeros(batch_size)
     for idx_batch in range(batch_size):
         s_optimal = s_optimal_batch[idx_batch]
         G_M = G_M_batch[idx_batch]
@@ -25,7 +27,8 @@ def worst_sinr_function(constants, s_optimal_batch, G_M_batch, H_M_batch):
             sinr[m,:] = numerator / denominator
         
         # Accumulate loss for each batch item
-        sum_worst_sinr += torch.min(sinr).item()
+        worst_sinr_batch[idx_batch] = torch.min(sinr).item()
+        # sum_worst_sinr += torch.min(sinr).item()
 
     # Average the loss over the batch
-    return sum_worst_sinr / batch_size
+    return worst_sinr_batch
