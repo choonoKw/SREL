a49221c0bacabcd40e8f49251e860459690a588f
Choono <49roinevery@gmail.com>
2024-03-28 09:57:45 -0400

    Create save_result_mat.py

---
+++ utils/save_result_mat.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 11:19:46 2024
+
+@author: jbk5816
+"""
+
+# import numpy as np
+from scipy.io import savemat
+
+# import os
+# import sys
+
+def save_mat(filename, worst_sinr_stack_list, f_stack_list):
+    """
+    Saves the given arrays to a .mat file.
+
+    Parameters:
+    - worst_sinr_stack_list: A numpy array
+    - f_stack_list: A numpy array
+    """
+    # Define the filename for the .mat file
+    
+    
+    # Create a dictionary of the variables to save
+    mat_contents = {
+        'worst_sinr_stack_list': worst_sinr_stack_list,
+        'f_stack_list': f_stack_list
+    }
+    
+    # Save the dictionary to a .mat file
+    
+    savemat(filename, mat_contents)
+    print(f"Data saved to {filename}")
