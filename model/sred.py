4a77056ba8a1c09459514a0bdefb07c516874fb4
Choono <49roinevery@gmail.com>
2024-03-29 13:44:52 -0400

    Update sred.py
    Batch Normalization option

--- model/sred.py
+++ model/sred.py
@@ -6,8 +6,9 @@ Created on Fri Mar  1 14:51:22 2024
 """
 
 from model.estimate_rho_sred import Estimate_rho
+from model.estimate_rho import Estimate_rho_BN
 
-from utils.input_standardize import standardize
+# from utils.input_standardize import standardize
 
 import torch
 import torch.nn as nn
@@ -255,6 +256,108 @@ class SRED_rep_rho(nn.Module):
             
             
         # return s_stack_batch
+            
+        model_outputs = {
+            's_stack_batch': s_stack_batch,
+            'rho_M_stack_batch': rho_M_stack_batch
+        }
+        return model_outputs
+
+def eta_sred_bn(s_batch,G_batch, H_batch):
+    s_batch_unsqueezed = s_batch.unsqueeze(-1)
+    
+    Gs_batch = torch.bmm(G_batch, s_batch_unsqueezed)
+    Hs_batch = torch.bmm(H_batch, s_batch_unsqueezed)
+    
+    sGs_batch = torch.einsum('bi,bi->b', s_batch.conj(), Gs_batch)
+    sHs_batch = torch.einsum('bi,bi->b', s_batch.conj(), Hs_batch)
+
+    
+    
+    # sGs = torch.vdot(s_batch, torch.matmul(G_batch, s_batch))  # Equivalent to s'*G*s in MATLAB
+    # sHs = torch.vdot(s_batch, torch.matmul(H_batch, s_batch))  # Equivalent to s'*H*s in MATLAB
+    # Gs = torch.matmul(G_batch, s_batch)  # G*s
+    # Hs = torch.matmul(H_batch, s_batch)  # H*s
+    eta_batch = torch.real(2 / (sHs_batch ** 2)
+                           * torch.imag((sHs_batch * Gs_batch - sGs_batch * Hs_batch) * torch.conj(s_batch)))
+    # for idx_batch in range(batch_size):
+    #     phi0 = phi_batch[idx_batch]
+    #     w_M = w_M_batch[idx_batch]
+    #     G_M = G_M_batch[idx_batch]
+    #     H_M = H_M_batch[idx_batch]
+        
+    #     eta_net = torch.zeros(self.Ls).to(self.device)
+    #     for m, (G, H) in enumerate(zip(torch.unbind(G_M, dim=2),
+    #                                        torch.unbind(H_M, dim=2))):
+    return eta_batch
+    
+    
+class SRED_rho_rep(nn.Module):
+    def __init__(self, constants):
+        super(SRED_rho_rep, self).__init__()
+        # Unpack constants from the dictionary and store as attributes
+        self.M = constants['M']
+        self.Ls = constants['Nt']*constants['N']
+        self.N_step = constants['N_step']
+        self.modulus = constants['modulus']
+        
+        # Dynamically create the modules for estimating eta and rho
+        self.est_rho_modules = Estimate_rho_BN(2*self.Ls + 2*constants['Lw'] + constants['Ly'], 1)
+        
+        
+    def forward(self, phi_batch, w_M_batch, y_M_batch, G_M_batch, H_M_batch):
+        batch_size = phi_batch.size(0)
+        N_step = self.N_step
+        modulus = self.modulus
+        # M = self.M
+        
+        # Initialize the list
+        s_stack_batch = torch.zeros(batch_size, N_step+1, self.Ls, dtype=torch.complex64).to(self.device)
+        rho_M_stack_batch = torch.zeros(batch_size, N_step, self.M).to(self.device)
+        
+        
+        # for idx_batch in range(batch_size):
+        #     phi = phi_batch[idx_batch]
+        #     w_M = w_M_batch[idx_batch]
+        #     G_M = G_M_batch[idx_batch]
+        #     H_M = H_M_batch[idx_batch]
+            
+            
+        # Repeat the update process N_step times
+        for update_step in range(N_step):
+            s_batch = modulus*torch.exp(1j *phi_batch)
+
+            eta_net_batch = torch.zeros(batch_size,self.Ls).to(self.device)
+
+            for m, (G_batch, H_batch) in enumerate(zip(torch.unbind(G_M_batch, dim=3),
+                                                       torch.unbind(H_M_batch, dim=3))):
+
+                w_batch = w_M_batch[:,:,m]
+                y_batch = y_M_batch[:,:,m]
+
+                # x = standardize(s, w, y)
+                x_batch = torch.cat((s_batch.real, s_batch.imag, w_batch.real, w_batch.imag, y_batch), dim=0)
+                print(torch.size(x_batch))
+                
+                eta_batch = eta_sred_bn(s_batch, G_M_batch, H_M_batch)
+
+                
+
+
+                rho_batch = self.est_rho_modules(x_batch)
+
+                eta_net_batch += rho_batch*eta_batch
+
+                rho_M_stack_batch[:,update_step,m] = rho_batch
+
+            phi_batch = phi_batch - eta_net_batch  # Update phi
+
+            # save on list
+            
+            s_stack_batch[:,update_step,:] = s_batch
+            # rho_stack_batch[idx_batch,update_step] = rho
+
+        s_stack_batch[:,N_step,:] = modulus*torch.exp(1j *phi_batch)  # Saving the final s after all updates
             
         model_outputs = {
             's_stack_batch': s_stack_batch,
