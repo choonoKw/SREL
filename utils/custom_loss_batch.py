df76e7c04f8f15120d494b05ba95c74ac6093228
Choono <49roinevery@gmail.com>
2024-04-02 15:07:49 -0400

    Properly training intra_phase1

---
+++ utils/custom_loss_batch.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  2 10:23:14 2024
+
+@author: jbk5816
+"""
+
+import torch
+
+def reciprocal_sinr(G_batch, H_batch, s_batch):
+    s_batch_unsqueezed = s_batch.unsqueeze(-1)
+    
+    
+    # I suspect this part might not be correct
+    Gs_batch = torch.bmm(G_batch, s_batch_unsqueezed).squeeze()
+    Hs_batch = torch.bmm(H_batch, s_batch_unsqueezed).squeeze()
+    
+    sGs_batch = torch.abs(torch.sum(torch.conj(s_batch) * Gs_batch, dim=1)).unsqueeze(-1)
+    sHs_batch = torch.abs(torch.sum(torch.conj(s_batch) * Hs_batch, dim=1)).unsqueeze(-1)
+     
+    return sGs_batch / sHs_batch
