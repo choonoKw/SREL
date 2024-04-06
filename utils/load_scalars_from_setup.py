67c368af81caa4b274d03300d5d56395a2a4e07b
Choono <49roinevery@gmail.com>
2024-04-06 01:14:28 -0400

    cleaning

--- utils/load_scalars_from_setup.py
+++ utils/load_scalars_from_setup.py
@@ -29,3 +29,25 @@ def load_scalars_from_setup(file_path):
     constants["Lw"] = constants['Lj']*constants['Nr']
     
     return constants
+
+def load_param_from_setup(file_path):
+    # Load the .mat file
+    data = loadmat(file_path, squeeze_me=True)
+    struct_c = data['struct_c']
+    
+class Struct_c:
+    def __init__(self, data):
+        struct_c = data['struct_c']
+        self.Nt = struct_c['Nt'].item()  # Number of transmitters
+        self.N = struct_c['N'].item()    # Number of time samples
+        self.Nr = struct_c['Nr'].item()  # Number of receivers
+        self.M = struct_c['M'].item()    # Number of targets
+        self.K = struct_c['K'].item()    # Number of interference
+        self.Lj = struct_c['Lj'].item()  # l_M - l_1 + N
+
+class Struct_k:
+    def __init__(self, data):
+        struct_k = data['struct_k']
+        self.r = struct_k['r']
+        self.sigma_k = struct_k['sigma_k']
+    
