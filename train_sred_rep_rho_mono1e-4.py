950ffc73225678c561df728f1b7fb617000051c7
Choono <49roinevery@gmail.com>
2024-04-08 11:44:48 -0400

    gpu1

--- train_sred_rep_rho_mono1e-4.py
+++ train_sred_rep_rho_mono1e-4.py
@@ -66,7 +66,7 @@ def main(save_weights, save_logs, save_mat,
     # Check for GPU availability.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if device.type == 'cuda':
-        torch.cuda.set_device(0)
+        torch.cuda.set_device(1)
     ###############################################################
     ## Control Panel
     ###############################################################
