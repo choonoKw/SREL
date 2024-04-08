e2bfda336b8e31744d2dd1393eb7f40fe0ab5ade
Choono <49roinevery@gmail.com>
2024-04-08 14:16:46 -0400

    save location

--- train_sred_rep_rho_mono1e-1.py
+++ train_sred_rep_rho_mono1e-1.py
@@ -103,7 +103,7 @@ def main(save_weights, save_logs, save_mat,
     
     # Create a unique directory name using the current time and the N_step value
     log_dir = (
-        f'runs/intra_phase1/rep_rho/data{data_num:.0e}/'
+        f'runs/sred_rep_rho/data{data_num:.0e}/'
         f'lambda_sinr{lambda_sinr:.0e}/'
         f'{start_time_tag}'
         f'_lambda_mono{lambda_mono:.0e}'
@@ -274,7 +274,7 @@ def main(save_weights, save_logs, save_mat,
     if save_mat:
         matfilename = "data_SRED_rho_10step_result.mat"
         dir_mat_save = (
-            f'mat/intra_phase1/rep_rho/{start_time_tag}'
+            f'mat/sred_rep_rho/{start_time_tag}'
             f'_Nstep{N_step:02d}_batch{batch_size:02d}'
             f'_sinr_{worst_sinr_avg_db:.2f}dB'
         )
@@ -292,7 +292,7 @@ def main(save_weights, save_logs, save_mat,
         }
         # save
         dir_weight_save = (
-            f'weights/intra_phase1/rep_rho/{start_time_tag}'
+            f'weights/sred_rep_rho/{start_time_tag}'
             f'_Nstep{N_step:02d}_batch{batch_size:02d}'
             f'_sinr_{worst_sinr_avg_db:.2f}dB'
         )
